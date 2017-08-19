d2ab39d4782b3bdb7e95a01632e1ef460a08debe
programmer-770 <hw.honnesh@gmail.com>
2017-08-19 14:42:28 +0530

    Update ReminderManager.java

--- app/src/main/java/com/honneshrohmetra/reminder/ReminderManager.java
+++ app/src/main/java/com/honneshrohmetra/reminder/ReminderManager.java
@@ -9,21 +9,21 @@ import android.content.Intent;
 
 public class ReminderManager {
 
-    private Context myContext;
-    private AlarmManager myAlarmManager;
+    private Context mContext;
+    private AlarmManager mAlarmManager;
 
     public ReminderManager(Context context) {
-        myContext = context;
-        myAlarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        mContext = context;
+        mAlarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
     }
 
     public void setReminder(Long taskId, Calendar when) {
 
-        Intent i = new Intent(myContext, OnAlarmReceiver.class);
+        Intent i = new Intent(mContext, OnAlarmReceiver.class);
         i.putExtra(RemindersDbAdapter.KEY_ROWID, (long)taskId);
 
-        PendingIntent pi = PendingIntent.getBroadcast(myContext, 0, i, PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pi = PendingIntent.getBroadcast(mContext, 0, i, PendingIntent.FLAG_ONE_SHOT);
 
-        myAlarmManager.set(AlarmManager.RTC_WAKEUP, when.getTimeInMillis(), pi);
+        mAlarmManager.set(AlarmManager.RTC_WAKEUP, when.getTimeInMillis(), pi);
     }
 }
